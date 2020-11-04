72b5d66c42fb26711d596413795ba3a9d3b4b313
jr1412594 <jr.medina1412@gmail.com>
2020-11-04 15:55:41 -0700

    create token

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,2 +1,17 @@
 class AuthenticationController < ApplicationController
+    def login
+        @user = User.find_by username: params[:username]
+
+        if !@user
+            render json: {message: "Incorrect username or password"}, 
+            status: :unauthorized
+        else
+            # byebug
+           if !@user.authenticate(params[:password])
+            render json: {message: "Incorrect username or password"}, 
+            status: :unauthorized
+           else
+        end
+        end
+    end
 end
